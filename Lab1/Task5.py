01aa52195179f9bff796dd4614687ee6a307b26e
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2024-02-07 16:07:58 +0300

    fix: Task5.py

--- Lab1/Task5.py
+++ Lab1/Task5.py
@@ -1,6 +1,7 @@
 from re import finditer
 
 
-def task5(s: str):
-    result = finditer(r"\d{1,2} (декабря|января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября) \d{4}", s)
+def task5(s: str) -> list[str]:
+    result = finditer(
+        r"\d{1,2} (декабря|января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября) \d{4}", s)
     return [x.group() for x in result]
