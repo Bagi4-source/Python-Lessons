8626702aec7a41102a5cb82e6d25975ed1f36d8f
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2024-02-08 11:53:48 +0300

    feat: Lab4 Task1

---
+++ Lab4/Task1.py
@@ -0,0 +1,13 @@
+def main():
+    with open("27-170b.txt", mode="r") as file:
+        lines = file.readlines()
+        numbers = list(map(int, lines[1:]))
+
+    n, k = map(int, lines[0].split(" "))
+    numbers = sorted(numbers)
+    result = sum(numbers[n - k - 1:n])
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
