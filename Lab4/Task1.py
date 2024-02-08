e61358ad8486124f756c0c980a5133da4278b432
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2024-02-08 12:18:35 +0300

    fix: Lab4 Task1

--- Lab4/Task1.py
+++ Lab4/Task1.py
@@ -1,9 +1,8 @@
 def main():
     with open("27-170b.txt", mode="r") as file:
-        lines = file.readlines()
-        numbers = list(map(int, lines[1:]))
+        n, k = map(int, file.readline().split(" "))
+        numbers = list(map(int, file.readlines()))
 
-    n, k = map(int, lines[0].split(" "))
     numbers.sort()
     result = sum(numbers[n - k - 1:n])
     print(result)
